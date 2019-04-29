9464e665d31df5bd923ffa7ff0344304f2a3d872
devinv <devinv@vt.edu>
2019-04-29 19:02:21 -0400

    Generalize search term

--- TobaccoTestimonies/app/views.py
+++ TobaccoTestimonies/app/views.py
@@ -6,12 +6,25 @@ import requests
 import json
 
 def get_documents(request):
+    ids = []
+    count = 0
     if request.method == 'GET':
         form = QuerySettingsForm(request.GET)
         if form.is_valid():
-            url = 'http://solr.industrydocumentslibrary.ucsf.edu/solr/ltdl3/query?q=author:' + request.GET['person'] + ' AND topic:' + request.GET['topic'] + '&wt=json'
+            url = 'http://solr.industrydocumentslibrary.ucsf.edu/solr/ltdl3/query?q=' + request.GET['topic'] + '&wt=json'
             result = json.loads(requests.get(url).content)
-            return render(request, 'httpResponse.html', {'documents': result['response']})
+            #documents = result['response']['docs']
+            #for doc in documents:
+                #count = count + 1
+                #ids.append(str(doc['id']))
+            #count = count + 1
+            return render(request, 'httpResponse.html', {'documents': result})
+            #docFound = result['response']['numFound']
+            #while count < docFound:
+                #if count % 100 == 0:
+                    #sec_url = 'http://solr.industrydocumentslibrary.ucsf.edu/solr/ltdl3/query?q=topic:' + request.GET['topic'] + '&start=' + str(count) + '&wt=json'
+                    #sec_result = json.loads(requests.get(sec_url).content)
+                    #sec_doc = sec_result['response']['docs']
 
     template_name = 'index.html'
     form = QuerySettingsForm()
